d3ad575f452f99e4aeb7677abf02876172284368
Yan <yan@example.com>
2026-08-26 12:44:53 +0200

    main.py

---
+++ Semester-5/OOP/practicle-projects/practic-7/build/CMakeFiles/practic_7.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/yan/Documents/Навчання/МНТУ/MNTU-edu-archive/Semester-5/OOP/practicle-projects/practic-7/clases/Menu.cpp" "CMakeFiles/practic_7.dir/clases/Menu.cpp.o" "gcc" "CMakeFiles/practic_7.dir/clases/Menu.cpp.o.d"
+  "/home/yan/Documents/Навчання/МНТУ/MNTU-edu-archive/Semester-5/OOP/practicle-projects/practic-7/clases/Object.cpp" "CMakeFiles/practic_7.dir/clases/Object.cpp.o" "gcc" "CMakeFiles/practic_7.dir/clases/Object.cpp.o.d"
+  "/home/yan/Documents/Навчання/МНТУ/MNTU-edu-archive/Semester-5/OOP/practicle-projects/practic-7/clases/Pyramid.cpp" "CMakeFiles/practic_7.dir/clases/Pyramid.cpp.o" "gcc" "CMakeFiles/practic_7.dir/clases/Pyramid.cpp.o.d"
+  "/home/yan/Documents/Навчання/МНТУ/MNTU-edu-archive/Semester-5/OOP/practicle-projects/practic-7/clases/QuadrangularPyramid.cpp" "CMakeFiles/practic_7.dir/clases/QuadrangularPyramid.cpp.o" "gcc" "CMakeFiles/practic_7.dir/clases/QuadrangularPyramid.cpp.o.d"
+  "/home/yan/Documents/Навчання/МНТУ/MNTU-edu-archive/Semester-5/OOP/practicle-projects/practic-7/clases/Sphere.cpp" "CMakeFiles/practic_7.dir/clases/Sphere.cpp.o" "gcc" "CMakeFiles/practic_7.dir/clases/Sphere.cpp.o.d"
+  "/home/yan/Documents/Навчання/МНТУ/MNTU-edu-archive/Semester-5/OOP/practicle-projects/practic-7/clases/TriangularPyramid.cpp" "CMakeFiles/practic_7.dir/clases/TriangularPyramid.cpp.o" "gcc" "CMakeFiles/practic_7.dir/clases/TriangularPyramid.cpp.o.d"
+  "/home/yan/Documents/Навчання/МНТУ/MNTU-edu-archive/Semester-5/OOP/practicle-projects/practic-7/clases/main.cpp" "CMakeFiles/practic_7.dir/clases/main.cpp.o" "gcc" "CMakeFiles/practic_7.dir/clases/main.cpp.o.d"
+  "" "practic_7" "gcc" "CMakeFiles/practic_7.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Semester-5/OOP/practicle-projects/practic-7/build/CMakeFiles/practic_7.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/practic_7.dir/link.d"
+  "CMakeFiles/practic_7.dir/clases/Menu.cpp.o"
+  "CMakeFiles/practic_7.dir/clases/Menu.cpp.o.d"
+  "CMakeFiles/practic_7.dir/clases/Object.cpp.o"
+  "CMakeFiles/practic_7.dir/clases/Object.cpp.o.d"
+  "CMakeFiles/practic_7.dir/clases/Pyramid.cpp.o"
+  "CMakeFiles/practic_7.dir/clases/Pyramid.cpp.o.d"
+  "CMakeFiles/practic_7.dir/clases/QuadrangularPyramid.cpp.o"
+  "CMakeFiles/practic_7.dir/clases/QuadrangularPyramid.cpp.o.d"
+  "CMakeFiles/practic_7.dir/clases/Sphere.cpp.o"
+  "CMakeFiles/practic_7.dir/clases/Sphere.cpp.o.d"
+  "CMakeFiles/practic_7.dir/clases/TriangularPyramid.cpp.o"
+  "CMakeFiles/practic_7.dir/clases/TriangularPyramid.cpp.o.d"
+  "CMakeFiles/practic_7.dir/clases/main.cpp.o"
+  "CMakeFiles/practic_7.dir/clases/main.cpp.o.d"
+  "practic_7"
+  "practic_7.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/practic_7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
