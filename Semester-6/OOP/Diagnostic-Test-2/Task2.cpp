86029a470ff5c29ff155b915130a323944657ec6
ShadowWatcher543 <mistercat7505@gmail.com>
2026-09-02 21:41:09 +0200

    Control work 2 (OOP)

--- Semester-6/OOP/Diagnostic-Test-2/Task2.cpp
+++ Semester-6/OOP/Diagnostic-Test-2/Task2.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
 
+template <typename T>
+class Stack {
+    vector<T> st;
+public:
+    void push(T val) { st.push_back(val); }
+    void pop() { if(!st.empty()) st.pop_back(); }
+
+    string getLongest() {
+        string maxStr = "";
+        for (auto& s : st) {
+            if (s.length() > maxStr.length()) maxStr = s;
+        }
+        return maxStr;
+    }
+};
+
+int main() {
+    Stack<string> st;
+    st.push("cpp");
+    st.push("some long string here");
+    st.push("hello");
+
+    cout << "Найдовший: " << st.getLongest();
+}
